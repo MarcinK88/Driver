59f30677126198af1cd6bf0de65151832b1b5ab6
Marcin <marcin3216@gmail.com>
2019-12-13 12:53:12 +0100

    advices and tags

--- src/main/java/pl/coderslab/model/Advice.java
+++ src/main/java/pl/coderslab/model/Advice.java
@@ -3,28 +3,29 @@ package pl.coderslab.model;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Set;
 
 @Entity
 @Data
+@Table(name = "advices")
 public class Advice {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-
     private String pathToImage;
     private String title;
-    private String lead;
+    private String leader;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
     private Long recommends;
-    private List<String> tags;
 
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "advice_tag", joinColumns = @JoinColumn(name = "advice_id"),
+        inverseJoinColumns = @JoinColumn(name = "tag_id"))
+    private Set<Tag> tags;
 
 }
